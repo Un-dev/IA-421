26e9926522403fdf0d3541276201b970c58dfe57
Un-dev <francois.lannoy@kelindi.com>
2021-05-04 14:12:50 +0200

    transition function

--- main.py
+++ main.py
@@ -4,51 +4,54 @@ import random
 STATES = [
   {
     "Récompense": 800,
-    "1": 1,
-    "2": 2,
-    "3": 4}, 
+    "0": 1,
+    "1": 2,
+    "2": 4}, 
   {
     "Récompense": 700,
+    "0": 1,
     "1": 1,
-    "2": 1,
-    "3": 1}, 
+    "2": 1}, 
   {
     "Récompense": 203,
-    "1": 1,
-    "2": 2,
-    "3": 3}, 
+    "0": 1,
+    "1": 2,
+    "2": 3}, 
   {
     "Récompense": 204,
-    "1": 2,
-    "2": 3,
-    "3": 4}, 
+    "0": 2,
+    "1": 3,
+    "2": 4}, 
   {
     "Récompense": 205,
-    "1": 3,
-    "2": 4,
-    "3": 5
+    "0": 3,
+    "1": 4,
+    "2": 5
   }, 
   {
     "Récompense": 206,
-    "1": 4,
-    "2": 5,
-    "3": 6
+    "0": 4,
+    "1": 5,
+    "2": 6
   }, 
   {
     "Récompense": 0,
-    "1": 1,
-    "2": 2,
-    "3": 2
+    "0": 1,
+    "1": 2,
+    "2": 2
   }]
 
 # def reward(state, action):
 
 def transition(state, action):
-  print(state[0])
-  # for i in action.item
-  #   if (item):
-  #     state[i] = random.randint(1,6)
-  # return state
+  # print(state["0"])
+  for i, item in enumerate(action):
+    if (item == True):
+      state[str(i)] = random.randint(1,6)
+
+  return state
 
 #avec l'etat 456 je relance le dé 2 et 3
-transition({"1": 4, "2": 5, "3": 6}, [False, True, True])
+
+print(transition({"0": 4, "1": 5, "2": 6}, [False, True, True]))
+
